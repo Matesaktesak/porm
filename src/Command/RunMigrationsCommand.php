42741ea46ba8df6992ee4507e5b7a11e8431e654
danik <me@subsonic.cz>
2018-03-31 01:41:31 +0200

    added dry-run option to porm:migrations:run

--- src/Command/RunMigrationsCommand.php
+++ src/Command/RunMigrationsCommand.php
@@ -26,20 +26,26 @@ class RunMigrationsCommand extends Command {
 
     protected function configure() : void {
         $this->setName('porm:migrations:run')
-            ->setDescription('Run any migrations that haven\'t been applied yet');
+            ->setDescription('Run any migrations that haven\'t been applied yet')
+            ->addOption('dry-run', 'd', 'Dry run (show which migrations haven\'t been applied yet)');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output) : int {
         $new = $this->resolver->getNewMigrations();
+        $dryRun = $input->getOption('dry-run');
+        $msg = $dryRun ? 'Migration %d (%s) hasn\'t been applied yet.' : 'Running migration %d (%s)...';
 
         if (!$new) {
             $output->writeln("No new migrations found.");
             return 0;
         } else {
             foreach ($new as $migration) {
-                $output->writeln(sprintf('Running migration %d (%s)...', $migration->version, $migration->type));
-                $this->runner->run($this->resolver->getPath($migration));
-                $this->resolver->markAsApplied($migration);
+                $output->writeln(sprintf($msg, $migration->version, $migration->type));
+
+                if (!$dryRun) {
+                    $this->runner->run($this->resolver->getPath($migration));
+                    $this->resolver->markAsApplied($migration);
+                }
             }
         }
 
