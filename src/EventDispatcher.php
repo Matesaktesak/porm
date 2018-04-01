7c9824834afe4b220c544ed94c5046be8cc19fe0
danik <me@subsonic.cz>
2018-04-01 21:34:34 +0200

    added the option to specify lazy event listeners using a callable

--- src/EventDispatcher.php
+++ src/EventDispatcher.php
@@ -20,7 +20,20 @@ class EventDispatcher {
     }
 
 
+    /**
+     * @param string $event
+     * @param object|string|callable $listener
+     * @param string|null $method
+     */
     public function addListener(string $event, $listener, ?string $method = null) : void {
+        if (is_string($listener)) {
+            if (!isset($this->listenerResolver)) {
+                throw new \InvalidArgumentException("Listener references are not supported when listener resolver is not set");
+            }
+        } else if (!is_object($listener) && !is_callable($listener)) {
+            throw new \InvalidArgumentException("Invalid listener, expected an object, string or callable, got " . gettype($listener));
+        }
+
         if (!$method) {
             $p = mb_strrpos($event, '::');
             $method = 'handle' . ucfirst($p !== false ? mb_substr($event, $p + 2) : $event);
@@ -43,8 +56,12 @@ class EventDispatcher {
     private function doDispatch(string $event, array $args) : void {
         if (!empty($this->listeners[$event])) {
             foreach ($this->listeners[$event] as $id => $listener) {
-                if (!is_object($listener[0]) && isset($this->listenerResolver)) {
-                    $this->listeners[$event][$id][0] = $listener[0] = call_user_func($this->listenerResolver, $listener[0]);
+                if (!is_object($listener[0])) {
+                    if (is_string($listener[0])) {
+                        $this->listeners[$event][$id][0] = $listener[0] = call_user_func($this->listenerResolver, $listener[0], $event);
+                    } else {
+                        $this->listeners[$event][$id][0] = $listener[0] = call_user_func($listener[0], $event);
+                    }
                 }
 
                 call_user_func_array($listener, $args);
